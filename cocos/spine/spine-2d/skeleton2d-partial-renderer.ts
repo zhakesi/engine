47db7b945852053b70d0ce88bcedc26873a4011c
liqiao <liqiao@cocos.com>
2023-03-31 19:25:07 +0800

    xxx

---
+++ cocos/spine/spine-2d/skeleton2d-partial-renderer.ts
@@ -0,0 +1,104 @@
+import { JSB } from 'internal:constants';
+import { Material, RenderingSubMesh, Texture2D } from '../../asset/assets';
+import { legacyCC } from '../../core/global-exports';
+import { ModelRenderer } from '../../misc/model-renderer';
+import { Model } from '../../render-scene/scene';
+import { ModelLocalBindings } from '../../rendering/define';
+import { vfmtPosUvColor, getAttributeStride, vfmtPosUvColor4B } from '../../2d/renderer/vertex-format';
+import { Root } from '../../root';
+import { BufferInfo, BufferUsageBit, Device, MemoryUsageBit, PrimitiveMode, deviceManager } from '../../gfx';
+import { builtinResMgr } from '../../asset/asset-manager';
+
+export class Skeleton2DPartialRenderer extends ModelRenderer {
+    private _texture: Texture2D | null = null;
+
+    constructor () {
+        super();
+        this.material = builtinResMgr.get<Material>('default-spine-material');
+    }
+
+    public onLoad () {
+        if (this._models.length < 1) {
+            this._createModel();
+        }
+    }
+
+    public onEnable () {
+        this._attachToScene();
+    }
+
+    public update (dt: number) {
+        console.log('Skeleton2DPartialRenderer:update');
+        //this._onUpdateLocalDescriptorSet();
+    }
+
+    protected _createModel () {
+        const model = (legacyCC.director.root as Root).createModel<Model>(Model);
+        this._models[0] = model;
+        model.visFlags = this.visibility;
+        model.node = model.transform = this.node;
+    }
+
+    protected _attachToScene () {
+        if (!this.node.scene || this._models.length < 1) {
+            return;
+        }
+        const renderScene = this._getRenderScene();
+        if (this._models[0].scene !== null) {
+            this._detachFromScene();
+        }
+        renderScene.addModel(this._models[0]);
+    }
+
+    protected _detachFromScene () {
+        if (this._models.length > 0 && this._models[0].scene) {
+            this._models[0].scene.removeModel(this._models[0]);
+        }
+    }
+
+    protected _onUpdateLocalDescriptorSet () {
+        if (!this._texture) return;
+        const subModels = this._models[0].subModels;
+        const binding = ModelLocalBindings.SAMPLER_SPRITE;
+        for (let i = 0; i < subModels.length; i++) {
+            const { descriptorSet } = subModels[i];
+            const texture = this._texture;
+            descriptorSet.bindTexture(binding, texture.getGFXTexture()!);
+            descriptorSet.bindSampler(binding, texture.getGFXSampler()!);
+            descriptorSet.update();
+        }
+    }
+
+    public _activeSubModel (idx: number) {
+        if (this._models.length < 1) {
+            return;
+        }
+        let attrs = vfmtPosUvColor;
+        if (JSB) {
+            attrs = vfmtPosUvColor4B;
+        }
+        const stride = getAttributeStride(attrs);
+
+        if (this._models[0].subModels.length <= idx) {
+            const gfxDevice: Device = deviceManager.gfxDevice;
+            const vertexBuffer = gfxDevice.createBuffer(new BufferInfo(
+                BufferUsageBit.VERTEX | BufferUsageBit.TRANSFER_DST,
+                MemoryUsageBit.DEVICE,
+                65535 * stride,
+                stride,
+            ));
+            const indexBuffer = gfxDevice.createBuffer(new BufferInfo(
+                BufferUsageBit.INDEX | BufferUsageBit.TRANSFER_DST,
+                MemoryUsageBit.DEVICE,
+                65535 * Uint16Array.BYTES_PER_ELEMENT * 2,
+                Uint16Array.BYTES_PER_ELEMENT,
+            ));
+
+            const renderMesh = new RenderingSubMesh([vertexBuffer], attrs, PrimitiveMode.TRIANGLE_LIST, indexBuffer);
+            renderMesh.subMeshIdx = 0;
+
+            this._models[0].initSubModel(idx, renderMesh, this.material!);
+            this._models[0].enabled = true;
+        }
+    }
+}
--- cocos/spine/spine-2d/skeleton2d-renderer.ts
+++ cocos/spine/spine-2d/skeleton2d-renderer.ts
@@ -23,15 +23,10 @@ import { ccclass, executeInEditMode, executionOrder, help, menu, serializable, t
 import { builtinResMgr } from '../../asset/asset-manager/builtin-res-mgr';
 import { PrimitiveMode, Device, BufferUsageBit, BufferInfo, MemoryUsageBit, deviceManager } from '../../gfx';
 import { Material, Texture2D, RenderingSubMesh } from '../../asset/assets';
-import { ModelRenderer } from '../../misc/model-renderer';
 import { editable } from '../../core/data/decorators';
 import { errorID } from '../../core/platform/debug';
 import { SkeletonData } from '../skeleton-data';
-import { ModelLocalBindings } from '../../rendering/define';
-import { vfmtPosUvColor, getAttributeStride, vfmtPosUvColor4B } from '../../2d/renderer/vertex-format';
-import { Model } from '../../render-scene/scene';
-import { Root } from '../../root';
-import { legacyCC } from '../../core/global-exports';
+
 import { ccenum, Enum } from '../../core/value-types/enum';
 import { CCClass } from '../../core/data/class';
 import { Skeleton2DImply } from './skeleton2d-imply';
@@ -39,8 +34,9 @@ import { Skeleton2DImplyWasm } from './skeleton2d-imply-wasm';
 import { Skeleton2DImplyNative } from './skeleton2d-imply-native';
 import { Skeleton2DMesh } from './skeleton2d-native';
 import { CCInteger } from '../../core';
-import { Node } from '../../scene-graph';
-import { Sprite } from '../../2d';
+import { Node } from '../../scene-graph/node';
+import { Skeleton2DPartialRenderer } from './skeleton2d-partial-renderer';
+import { Component } from '../../scene-graph';
 
 export enum SkelSkinsEnum {
     default = 0,
@@ -82,7 +78,7 @@ SkeletonData.prototype['init'] = function () {
 @executionOrder(100)
 @menu('2D/Skeleton2DRenderer')
 @executeInEditMode
-export class Skeleton2DRenderer extends ModelRenderer {
+export class Skeleton2DRenderer extends Component {
     @serializable
     protected _skeletonData: SkeletonData | null = null;
     @serializable
@@ -93,6 +89,7 @@ export class Skeleton2DRenderer extends ModelRenderer {
     private _texture: Texture2D | null = null;
     @serializable
     private _separatorsNum = 0;
+    private _parts: Skeleton2DPartialRenderer[] = [];
 
     private _imply: Skeleton2DImply | null = null;
     private _meshArray: Skeleton2DMesh[] = [];
@@ -110,6 +107,7 @@ export class Skeleton2DRenderer extends ModelRenderer {
 
     @editable
     @type(SkeletonData)
+    @displayName('SkeletonData')
     get skeletonData () {
         return this._skeletonData;
     }
@@ -218,13 +216,25 @@ export class Skeleton2DRenderer extends ModelRenderer {
     }
     set separatorsNumber (val: number) {
         this._separatorsNum = val;
-        this.node.removeAllChildren();
-        let i = 0;
-        for (i = 0; i < this._separatorsNum; i++) {
-            const node  = new Node(i.toString());
-            node.addComponent(Sprite);
-            node.parent = this.node;
-        }
+        // const children = this.node.children;
+        // let count = children.length;
+        // let i = 0;
+        // for (; i < count; i++) {
+        //     const child = children[i];
+        //     const part = child.getComponent(Skeleton2DPartialRenderer);
+        //     if (part) {
+        //         child.parent = null;
+        //         child.destroy();
+        //         i--;
+        //         count--;
+        //     }
+        // }
+        // i = 0;
+        // for (i = 0; i <= this._separatorsNum; i++) {
+        //     const node = new Node(`${i.toString()}-part`);
+        //     node.addComponent(Skeleton2DPartialRenderer);
+        //     node.parent = this.node;
+        // }
     }
 
     public setSkin (skinName: string) {
@@ -271,12 +281,6 @@ export class Skeleton2DRenderer extends ModelRenderer {
         }
     }
 
-    public onLoad () {
-        if (this._models.length < 1) {
-            this._createModel();
-        }
-    }
-
     public onRestore () {
         // if (!this._wasmObj) return;
         // console.log('onRestore');
@@ -286,14 +290,14 @@ export class Skeleton2DRenderer extends ModelRenderer {
         if (!this._imply) return;
         if (!EDITOR) this._imply.updateAnimation(dt);
         this._meshArray = this._imply.updateRenderData();
+        console.log('Skeleton2DRenderer:update');
 
-        this.realTimeTraverse();
-        this._onUpdateLocalDescriptorSet();
+        //this.realTimeTraverse();
     }
 
     public onEnable () {
-        this._attachToScene();
         this._updateSkeletonData();
+        this._resetPartialSetting();
     }
 
     public onDisable () {
@@ -318,90 +322,32 @@ export class Skeleton2DRenderer extends ModelRenderer {
         this._imply.setAnimation(name);
     }
 
-    protected _onUpdateLocalDescriptorSet () {
-        if (!this.texture) return;
-        const subModels = this._models[0].subModels;
-        const binding = ModelLocalBindings.SAMPLER_SPRITE;
-        for (let i = 0; i < subModels.length; i++) {
-            const { descriptorSet } = subModels[i];
-            const texture = this._texture;
-            descriptorSet.bindTexture(binding, texture!.getGFXTexture()!);
-            descriptorSet.bindSampler(binding, texture!.getGFXSampler()!);
-            descriptorSet.update();
+    private _resetPartialSetting () {
+        if (this._separatorsNum < 1) {
+            this._parts.length = 0;
+            let part = this.node.getComponent(Skeleton2DPartialRenderer);
+            if (part) {
+                this.node.removeComponent(Skeleton2DPartialRenderer);
+            }
+            part = this.node.addComponent(Skeleton2DPartialRenderer);
+            this._parts.push(part);
         }
     }
 
-    protected _createModel () {
-        const model = (legacyCC.director.root as Root).createModel<Model>(Model);
-        this._models[0] = model;
-        model.visFlags = this.visibility;
-        model.node = model.transform = this.node;
-    }
+    // public realTimeTraverse () {
+    //     const count = this._meshArray.length;
+    //     for (let idx = 0;  idx < count; idx++) {
+    //         const mesh = this._meshArray[idx];
 
-    protected _attachToScene () {
-        if (!this.node.scene || this._models.length < 1) {
-            return;
-        }
-        const renderScene = this._getRenderScene();
-        if (this._models[0].scene !== null) {
-            this._detachFromScene();
-        }
-        renderScene.addModel(this._models[0]);
-    }
-
-    protected _detachFromScene () {
-        if (this._models.length > 0 && this._models[0].scene) {
-            this._models[0].scene.removeModel(this._models[0]);
-        }
-    }
-
-    public _activeSubModel (idx: number) {
-        if (this._models.length < 1) {
-            return;
-        }
-        let attrs = vfmtPosUvColor;
-        if (JSB) {
-            attrs = vfmtPosUvColor4B;
-        }
-        const stride = getAttributeStride(attrs);
-
-        if (this._models[0].subModels.length <= idx) {
-            const gfxDevice: Device = deviceManager.gfxDevice;
-            const vertexBuffer = gfxDevice.createBuffer(new BufferInfo(
-                BufferUsageBit.VERTEX | BufferUsageBit.TRANSFER_DST,
-                MemoryUsageBit.DEVICE,
-                65535 * stride,
-                stride,
-            ));
-            const indexBuffer = gfxDevice.createBuffer(new BufferInfo(
-                BufferUsageBit.INDEX | BufferUsageBit.TRANSFER_DST,
-                MemoryUsageBit.DEVICE,
-                65535 * Uint16Array.BYTES_PER_ELEMENT * 2,
-                Uint16Array.BYTES_PER_ELEMENT,
-            ));
-
-            const renderMesh = new RenderingSubMesh([vertexBuffer], attrs, PrimitiveMode.TRIANGLE_LIST, indexBuffer);
-            renderMesh.subMeshIdx = 0;
-
-            const mat = builtinResMgr.get<Material>('default-spine-material');
-            this._models[0].initSubModel(idx, renderMesh, mat);
-            this._models[0].enabled = true;
-        }
-    }
-    public realTimeTraverse () {
-        const count = this._meshArray.length;
-        for (let idx = 0;  idx < count; idx++) {
-            const mesh = this._meshArray[idx];
-
-            this._activeSubModel(idx);
-            const subModel = this._models[0].subModels[idx];
-            const ia = subModel.inputAssembler;
-            const vb = new Float32Array(mesh.vertices);
-            ia.vertexBuffers[0].update(vb);
-            ia.vertexCount = mesh.vCount;
-            const ib = new Uint16Array(mesh.indices);
-            ia.indexBuffer!.update(ib);
-            ia.indexCount = ib.length;
-        }
-    }
+    //         this._activeSubModel(idx);
+    //         const subModel = this._models[0].subModels[idx];
+    //         const ia = subModel.inputAssembler;
+    //         const vb = new Float32Array(mesh.vertices);
+    //         ia.vertexBuffers[0].update(vb);
+    //         ia.vertexCount = mesh.vCount;
+    //         const ib = new Uint16Array(mesh.indices);
+    //         ia.indexBuffer!.update(ib);
+    //         ia.indexCount = ib.length;
+    //     }
+    // }
 }
