166d28d1ab8fa85260656dd8b2855fa1469b0f9d
liqiao <zhakesi@foxmail.com>
2023-06-03 10:28:37 +0800

    remove files

--- native/cocos/editor-support/spine-wasm/share-mem.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SPINE_WASM_SHARE_MEM_H__
-#define __SPINE_WASM_SHARE_MEM_H__
-#include <stdint.h>
-
-uint8_t* getStoreMemory();
-void     freeStoreMemory();
-uint32_t storeMemorySize();
-
-#endif
